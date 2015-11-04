2b5e2d8c8902590e7c31c65539db2c53a882096f
Braelan <bbraelan@gmail.com>
2015-11-04 13:47:08 -0800

    added downvote route

--- routes/index.js
+++ routes/index.js
@@ -66,6 +66,12 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
+router.put('/posts/:post/downvote', function(req,res, next){
+  req.post.downvote(function(err, post){
+    if(err) {return next(err);}
+    res.json(post);
+  });
+});
 
 //POSTS COMMENTS ROUTERS
 router.get('/posts/:post/comments', function(req, res, next){
